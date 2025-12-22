0db61443befbe8d7734e57334cbda0dbbe9cac66
ruduba <vatbudur@gmail.com>
2025-12-22 20:34:15 +0530

    watermelon.cpp

---
+++ watermelon.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+int n = 0;
+cin>>n;
+
+if((n-2)%2 == 0 && (n-2)>0){
+cout<<"YES"<<" ";
+}else{
+cout<<"NO"<<" ";
+}
+
+return 0;
+}
