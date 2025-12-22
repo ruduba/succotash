ca91269ca615bacb11821332c8501b21c9d92947
ruduba <vatbudur@gmail.com>
2025-12-22 21:10:17 +0530

    way too long words

---
+++ way-too-long-words.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int n;
+    cin>>n;
+    cout<<"\n";
+    string strings[n];
+    for(int i = 0; i<n; i++){
+        cin>>strings[i];
+    }
+    for(int i = 0; i<n; i++){
+        long long siz = strings[i].length();
+        if(siz>10){
+            cout<<strings[i][0] + to_string(siz-2) + strings[i][siz-1] <<" ";
+        }else{
+            cout<<strings[i]<<" ";
+        }
+        cout<<"\n";
+    }
+    puts("");
+    return 0;
+}
