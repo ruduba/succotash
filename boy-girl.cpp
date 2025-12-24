643b19ff1cea2f88770f20f79090a332e64495ed
ruduba <vatbudur@gmail.com>
2025-12-24 21:56:26 +0530

    354967367    Dec/24/2025 21:55UTC+5.5    succotash    236A - Boy or Girl    C++20 (GCC 13-64)    Accepted    92 ms    0 KB

---
+++ boy-girl.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  
+  string s;
+  cin>>s;
+  
+  sort(s.begin(), s.end());
+  
+  int tmp = unique(s.begin(), s.end()) -  s.begin();
+  
+  if(tmp%2 == 0){
+      cout<<"CHAT WITH HER!";
+  }else{
+      cout<<"IGNORE HIM!";
+  }
+  
+  return 0;
+}
