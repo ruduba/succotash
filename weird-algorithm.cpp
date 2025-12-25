3dc79441febc50e5bbe02f092ba3316bf09459e8
ruduba <vatbudur@gmail.com>
2025-12-25 23:14:31 +0530

    accepted

---
+++ weird-algorithm.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    long long n;
+    cin>>n;
+    
+    if(n<=1){
+        cout<<n<<" ";
+        return 0;
+    } 
+    
+    cout<<n<<" ";
+    while(n!=1){
+        
+        
+        if(n%2 == 0) n/=2;
+        else n = (n*3)+1;
+        cout<<n<<" ";
+    }
+    return 0;
+    
+}
