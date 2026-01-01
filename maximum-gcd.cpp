d04f82c990dad05a9962fb8e0248260109310dcf
ruduba <vatbudur@gmail.com>
2026-01-01 21:52:29 +0530

    356083958    Jan/01/2026 21:51UTC+5.5    succotash    1370A - Maximum GCD    C++20 (GCC 13-64)    Accepted    31 ms    0 KB

---
+++ maximum-gcd.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(){
+    
+    int t, n = 0;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        cout<<n/2<<"\n";
+    }
+    
+    return 0;
+    
+}
