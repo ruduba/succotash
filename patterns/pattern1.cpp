b1a76d5e4494a6c7e47d8f10f8207b3312f90381
ruduba <vatbudur@gmail.com>
2026-01-04 17:58:10 +0530

    patterns

---
+++ patterns/pattern1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int x = 5, y = 5;
+    
+    for(int i = 1; i<=x; i++){
+        for(int j = 1; j<=y; j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    
+}
