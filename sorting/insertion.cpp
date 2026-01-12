39a62360eaa5b5b9f3b1177f3601c861e4eb5ca0
ruduba <vatbudur@gmail.com>
2026-01-12 11:38:24 +0530

    O(N)

---
+++ sorting/insertion.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(){
+    
+    int n = 5;
+    vector<int> ar = {10, 3, 2, 8, 4};
+    
+    for(auto x: ar){
+        cout<<x<<" ";
+    }
+    cout<<"\n";
+    
+    int key, j;
+    
+    for(int i = 1; i<n; i++){
+        
+        key = ar[i];
+        j = i;
+        
+        while(j > 0 && ar[j-1] > key){
+            ar[j] = ar[j-1];
+            j--;
+        }
+        
+        ar[j] = key;
+        for(auto x: ar){
+        cout<<x<<" ";
+    }
+    cout<<"\n";
+    }
+    
+    return 0;
+}
