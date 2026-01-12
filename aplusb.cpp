6464a24b4dadcbdf14b1ac0a1c0d3ba8f2b146f4
ruduba <vatbudur@gmail.com>
2026-01-13 00:12:06 +0530

    357626481    Jan/13/2026 00:11UTC+5.5    succotash    A - A+B?    C++20 (GCC 13-64)    Accepted    46 ms    100 KB

---
+++ aplusb.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main(){
+    
+    
+    int t;
+    cin>>t;
+    
+    while(t--){
+        string ex;
+        cin>>ex;
+        
+        int a = atoi(&ex[0]);
+        int b = atoi(&ex[2]);
+        
+        cout<< a+b<<"\n";
+        
+        
+        
+    }
+
+    return 0;
+}
