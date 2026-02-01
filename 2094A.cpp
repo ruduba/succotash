8d22de561906dd4dced6909f220c3958d685b8bd
ruduba <vatbudur@gmail.com>
2026-02-01 22:54:16 +0530

    361035485    Feb/01/2026 22:53UTC+5.5    succotash    2094A - Trippi Troppi    C++20 (GCC 13-64)    Accepted    15 ms    0 KB

---
+++ 2094A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int t;
+    cin>>t;
+    
+    while(t--){
+        string w;
+        for(int i = 0; i<3; i++){
+            string p;
+            cin>>p;
+            
+            w += p[0];
+
+        }
+       
+    cout<<w<<"\n";
+    }
+    return 0;
+}
