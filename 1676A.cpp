4269994ab0e1386b76348bbee1e57982cfbb378b
vatsala budur <114461494+ruduba@users.noreply.github.com>
2026-02-05 23:33:06 +0530

    Modified, accepted

--- 1676A.cpp
+++ 1676A.cpp
@@ -1,26 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-
+int main() {
     int t;
-    cin>>t;
-    
-    while(t--){
+    cin >> t;
+
+    while (t--) {
         string s;
-        cin>>s;
+        cin >> s;
 
         int a = 0, b = 0;
-        
-        for(int i = 0; i<s.size()/2; i++){
-            a += (s[i] - '0');
-            b += (s[i+3] - '0');
-        }
-        
-        cout<< (a == b ? "YES" : "NO" ) <<"\n";
+        int n = s.size();
 
+        for (int i = 0; i < n / 2; i++) {
+            a += (s[i] - '0');
+            b += (s[i + n / 2] - '0');
+        }
+
+        cout << (a != b ? "NO" : "YES") << "\n";
     }
-    
-    
-    
+
+    return 0;
 }
