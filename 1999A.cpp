55acdcfd62b7453cdac3db8b4828b228b9f56c1a
ruduba <vatbudur@gmail.com>
2026-02-09 23:06:33 +0530

    362168109    Feb/09/2026 23:05UTC+5.5    succotash    1999A - A+B Again?    C++20 (GCC 13-64)    Accepted    15 ms    0 KB

---
+++ 1999A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int t;
+    cin>>t;
+    
+    while(t--){
+        char a, b;
+        cin>>a>>b;
+        
+        int sum = (a - '0') + (b - '0');
+        
+        cout<<sum<<"\n";
+    
+    }
+    return 0;
+}
