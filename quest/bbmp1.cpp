ba68156417ef29ee2e159b09c32fa86eba6452c6
ruduba <vatbudur@gmail.com>
2026-02-14 23:14:07 +0530

    accepted

---
+++ quest/bbmp1.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string yes(int n, string s){
+  unordered_set<char> t = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+  string ya;
+  for(auto x: s){
+    if(t.find(x)== t.end()){
+      ya+=x;
+    }
+  }
+
+  return ya;
+}
+
+
+
+int main(){
+  int t;
+  cin>>t;
+
+
+  while(t--){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+
+    string ans = yes(n, s);
+    cout<<ans<<"\n";
+  }
+
+
+return 0;
+}
