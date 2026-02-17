c428950f8546b66df54b3f651d45d76c7292e1e9
ruduba <vatbudur@gmail.com>
2026-02-17 18:21:01 +0530

    accepted

--- lc/symmetric-tree.cpp
+++ lc/symmetric-tree.cpp
@@ -9,3 +9,19 @@ using namespace std;
      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+
+class Solution {
+public:
+    bool test(TreeNode* a, TreeNode* b){
+        if(!a && !b) return true;
+        else if(!a || !b) return false;
+        else if(a->val != b->val) return false;
+        return test(a->left, b->right) && test(a->right, b->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if(!root) return false;
+
+        return test(root->left, root->right);
+    }
+};
