ca7515ecd6e8d21e8c44cf780d41d04402109b9b
ruduba <vatbudur@gmail.com>
2026-02-17 18:21:12 +0530

    accepted

---
+++ lc/tree-balanced.cpp
@@ -0,0 +1,27 @@
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+class Solution {
+public:
+    bool balanced = true;
+    int dfs(TreeNode* p){
+        if(!p) return 0;
+        int l = dfs(p->left);
+        int r = dfs(p->right);
+        if(abs(l-r)>1) balanced = false;
+
+        return max(l,r) + 1;
+    }
+
+    bool isBalanced(TreeNode* root) {
+        dfs(root);
+
+        return balanced;
+    }
+};
