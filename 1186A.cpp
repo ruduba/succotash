54ecebd6b02fea3485bd1ea17dc8e9f2427aa322
ruduba <vatbudur@gmail.com>
2026-02-19 23:30:26 +0530

    363646878    Feb/19/2026 23:29UTC+5.5    succotash    1186A - Vus the Cossack and a Contest    C++20 (GCC 13-64)    Accepted    31 ms    100 KB

---
+++ 1186A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, a, b;
+    cin>>n>>a>>b;
+    
+    int tmp = min(a, b);
+    
+    if(tmp<n){
+        cout<<"No";
+    }else{
+        cout<<"Yes";
+    }
+    
+    
+    
+    
+}
