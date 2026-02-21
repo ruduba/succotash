3662a0d13a916ffb74f8d844ef6057ede2859b72
ruduba <vatbudur@gmail.com>
2026-02-21 22:02:09 +0530

    accepted

---
+++ c/print-alp.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    
+    for(int i = 65; i<=90; i++){
+        printf("%c ", (char)i);
+    }
+    printf("\n");
+        for(int i = 97; i<=122; i++){
+        printf("%c ", (char)i);
+    }
+
+
+return 0;
+}
