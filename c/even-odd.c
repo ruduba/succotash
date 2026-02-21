bebfffcaa44e8672d5ca570dd3c8ee6e39979a57
ruduba <vatbudur@gmail.com>
2026-02-21 22:01:28 +0530

    accepted

---
+++ c/even-odd.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+int main(){
+    
+    int n;
+    scanf("%d", &n);
+    
+    if(n%2==0){
+        printf("%d is Even", n);
+    }else{
+        printf("%d is Odd", n);
+    }
+
+
+return 0;
+}
