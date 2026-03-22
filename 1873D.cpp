fc96d71df70711239eb898eff13d4044753a981c
ruduba <vatbudur@gmail.com>
2026-03-22 21:48:31 +0530

    367806534    Mar/22/2026 21:47UTC+5.5    succotash    D - 1D Eraser    C++20 (GCC 13-64)    Accepted    62 ms    0 KB

---
+++ 1873D.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n, k, res = 0;
+        cin>>n>>k;
+        
+        string s;
+        cin>>s;
+        
+        for(int i = 0; i<n; i++){
+            if(s[i] =='B'){
+                res++;
+                i += k-1;
+            }
+        }
+        
+        cout<<res<<"\n";
+    }
+    
+    
+    
+}
